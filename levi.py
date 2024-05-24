880da4b76e1ae15514f81471dad1a0e42b4a2b7e
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-05-25 01:35:11 +0530

    Update levi.py

--- levi.py
+++ levi.py
@@ -4,39 +4,34 @@
 
 
 
+
 from pyrogram import Client, filters, idle
+from pyrogram import Client as app
 import requests
 import random
 
-api_id = 29422639
+
+api_id =  29422639
 api_hash = 'e21bccfd64a01c5762ce81c77379dc7f' 
 
 token = '6368969091:AAF-gE8KSbodGuCYIzIvHWYWk0LXjW98pvw' #--Enter Bot Token Here.
 
 
-START_MSG = """
-**Hyy{},
-This is a basic bot to react on your messages. 
-Use /react while replying on someone's message.**
-"""
-
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
-
-@app.on_message(filters.command('start'))
-async def start_cmd(client, message):
-    await message.reply(START_MSG.format(message.from_user.mention))
 
 emojis = ["👍", "👎", "❤️", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊", "🤡", "🥱", "🥴", "😍", "🐳", "❤️‍🔥", "🌚", "🌭", "💯", "🤣", "⚡️", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍️", "🤗", "🫡", "🎅", "🎄", "☃️", "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂️", "🤷", "🤷‍♀️", "😡"]
 
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
 
-@app.on_message()
+@app.on_message(filters.command("react"))
 async def react_to_message(client, message):
-    chat_id = message.chat.id
-    message_id = message.id
+    reply = message.reply_to_message
+    chat_id = reply.chat.id
+    message_id = reply.id
+
 
     random_emoji = random.choice(emojis)
 
-    url = f'https://api.telegram.org/bot{token}/setReaction'
+    url = f'https://api.telegram.org/bot{token}/setMessageReaction'
 
 
     params = {
