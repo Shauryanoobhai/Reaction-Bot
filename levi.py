3cdb9cb18c647e3fd6ab08913c6852dc8169455d
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-05-25 01:22:37 +0530

    Update levi.py

--- levi.py
+++ levi.py
@@ -2,13 +2,13 @@
 
 
 
+
 from pyrogram import Client, filters, idle
 
-api_id =  29422639
+api_id = 29422639
 api_hash = 'e21bccfd64a01c5762ce81c77379dc7f' 
 
 token = '6368969091:AAF-gE8KSbodGuCYIzIvHWYWk0LXjW98pvw' #--Enter Bot Token Here.
-
 
 
 START_MSG = """
@@ -17,20 +17,20 @@ This is a basic bot to react on your messages.
 Use /react while replying on someone's message.**
 """
 
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
+
 @app.on_message(filters.command('start'))
 async def start_cmd(client, message):
     await message.reply(START_MSG.format(message.from_user.mention))
 
 emojis = ["👍", "👎", "❤️", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊", "🤡", "🥱", "🥴", "😍", "🐳", "❤️‍🔥", "🌚", "🌭", "💯", "🤣", "⚡️", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍️", "🤗", "🫡", "🎅", "🎄", "☃️", "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂️", "🤷", "🤷‍♀️", "😡"]
 
-bot = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
 
-@bot.on_message(filters.command("react"))
+@app.on_message(filters.command("react"))
 async def react_to_message(client, message):
     reply = message.reply_to_message
     chat_id = reply.chat.id
     message_id = reply.id
-
 
     random_emoji = random.choice(emojis)
 
@@ -56,6 +56,6 @@ async def react_to_message(client, message):
         print("Response content:", response.content)
 
 print("starting...")
-bot.start()
+app.start()
 
 idle()
